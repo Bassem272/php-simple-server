c789acf96648f0ec0c27d5081ad9703e2ffbb44e
bassem <bassem.gehad95@yahoo.com>
2025-01-19 14:38:07 +0200

    ggg

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
+// Get the port assigned by Railway, default to 8080 if not found
+$port = getenv('PORT') ?: 8080;
+
+// Start a simple PHP server on the correct port
+echo "Hello from PHP server!";
+
 // A very basic response to check if the server is working
 echo "Hello from PHP server!";
